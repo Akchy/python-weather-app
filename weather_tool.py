a484b5a1fd50afa38b92f5932bf30d3b1f1d8b38
Murali Krishnan A <amuralikrishnan@outlook.com>
2020-08-05 12:02:20 +0530

    Modify weather_tool.py

--- weather_tool.py
+++ weather_tool.py
@@ -7,7 +7,9 @@ city = input("Enter name of city")
 url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=***Enter your API Key here***&units=metric".format(city)
 res = requests.get(url)
 data = res.json()
-#pprint(data) This is to print the dictionary in a pretty way to understand it better
+
+pprint(data) #This is to print the dictionary in a pretty way to understand it better
+
 temp_c = data['main']['temp']
 temp_f = temp_c*1.8+32
 weather = data['weather'][0]['main']
